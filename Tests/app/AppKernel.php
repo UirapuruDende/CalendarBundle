7d299269298325e701686d34ac57d12e6a64e9d2
Grzegorz Kaszuba <uirapuruadg@gmail.com>
2015-10-17 22:42:50 +0200

    mapping

--- Tests/app/AppKernel.php
+++ Tests/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Dende\CalendarBundle\DendeCalendarBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle()
         );
     }
 
