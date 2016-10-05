6e60811a58751058d9fc329facc403e3391d3228
Grzegorz Kaszuba <uirapuruadg@gmail.com>
2016-10-05 23:56:07 +0100

    up

--- Tests/Functional/Controller/DefaultControllerTest.php
+++ Tests/Functional/Controller/DefaultControllerTest.php
@@ -17,11 +17,6 @@ class DefaultControllerTest extends BaseFunctionalTest
     const FORMAT_DATETIME = "Y-m-d H:i";
 
     /**
-     * @var Calendar
-     */
-    private $calendar;
-
-    /**
      * @var EntityManager
      */
     private $em;
@@ -31,7 +26,6 @@ class DefaultControllerTest extends BaseFunctionalTest
         parent::setUp();
 
         $this->em = $this->container->get("doctrine.orm.entity_manager");
-        $this->calendar = $this->em->getRepository(Calendar::class)->findOneByName('Brazilian Jiu Jitsu');
     }
 
     public function tearDown()
@@ -56,13 +50,16 @@ class DefaultControllerTest extends BaseFunctionalTest
      */
     public function adding_new_single_event()
     {
+        /** @var Calendar $calendar */
+        $calendar = $this->em->getRepository(Calendar::class)->findOneByName('Brazilian Jiu Jitsu');
+
         $crawler = $this->client->request('GET', '/calendar/occurrence/new');
         $this->assertResponseCode();
 
         $form = $crawler->selectButton('dende_calendar.form.submit.label')->form();
 
         $form->setValues([
-            "create_event[calendar]" => $this->calendar->id(),
+            "create_event[calendar]" => $calendar->id(),
             "create_event[type]" => Calendar\Event\EventType::TYPE_SINGLE,
             "create_event[startDate]" => "2015-11-02 12:00",
             "create_event[endDate]" => "2015-11-02 13:30",
@@ -75,14 +72,15 @@ class DefaultControllerTest extends BaseFunctionalTest
         $this->assertResponseCode();
         $this->assertEquals("/calendar/", $this->client->getRequest()->getRequestUri());
 
-        $this->em->refresh($this->calendar);
+        /** @var Calendar $calendar */
+        $calendar = $this->em->getRepository(Calendar::class)->find($calendar->id());
 
         /** @var Event $event */
-        $event = $this->calendar->events()->get(1);
+        $event = $calendar->events()->get(1);
         /** @var Occurrence $occurrence */
         $occurrence = $event->occurrences()->first();
 
-        $this->assertCount(2, $this->calendar->events());
+        $this->assertCount(2, $calendar->events());
         $this->assertCount(1, $event->occurrences());
         $this->assertEquals('test-event-title', $event->title());
         $this->assertEquals('single', $event->type()->type());
@@ -101,6 +99,9 @@ class DefaultControllerTest extends BaseFunctionalTest
      */
     public function adding_new_weekly_event()
     {
+        /** @var Calendar $calendar */
+        $calendar = $this->em->getRepository(Calendar::class)->findOneByName('Brazilian Jiu Jitsu');
+
         $crawler = $this->client->request('GET', '/calendar/occurrence/new');
 
         $this->assertResponseCode();
@@ -108,7 +109,7 @@ class DefaultControllerTest extends BaseFunctionalTest
         $form = $crawler->selectButton('dende_calendar.form.submit.label')->form();
 
         $form->setValues([
-            "create_event[calendar]" => $this->calendar->id(),
+            "create_event[calendar]" => $calendar->id(),
             "create_event[type]" => Calendar\Event\EventType::TYPE_WEEKLY,
             "create_event[startDate]" => "2015-09-01 12:00",
             "create_event[endDate]" => "2015-09-30 13:30",
@@ -124,12 +125,13 @@ class DefaultControllerTest extends BaseFunctionalTest
         $this->assertResponseCode();
         $this->assertEquals("/calendar/", $this->client->getRequest()->getRequestUri());
 
-        $this->em->refresh($this->calendar);
+        /** @var Calendar $calendar */
+        $calendar = $this->em->getRepository(Calendar::class)->find($calendar->id());
 
         /** @var Event $event */
         $event = $this->em->getRepository(Event::class)->findOneByTitle('Test weekly event 1');
 
-        $this->assertCount(2, $this->calendar->events());
+        $this->assertCount(2, $calendar->events());
         $this->assertCount(13, $event->occurrences());
         $this->assertEquals('Test weekly event 1', $event->title());
         $this->assertEquals('weekly', $event->type()->type());
@@ -174,6 +176,9 @@ class DefaultControllerTest extends BaseFunctionalTest
      */
     public function adding_new_weekly_event_to_a_new_calendar()
     {
+        /** @var Calendar $calendar */
+        $calendar = $this->em->getRepository(Calendar::class)->findOneByName('Brazilian Jiu Jitsu');
+
         $crawler = $this->client->request('GET', '/calendar/occurrence/new');
 
         $this->assertResponseCode();
@@ -181,7 +186,7 @@ class DefaultControllerTest extends BaseFunctionalTest
         $form = $crawler->selectButton('dende_calendar.form.submit.label')->form();
 
         $form->setValues([
-            "create_event[calendar]" => $this->calendar->id(),
+            "create_event[calendar]" => $calendar->id(),
             "create_event[newCalendarName]" => 'i am new calendar added',
             "create_event[type]" => Calendar\Event\EventType::TYPE_WEEKLY,
             "create_event[startDate]" => "2015-09-01 12:00",
@@ -198,7 +203,7 @@ class DefaultControllerTest extends BaseFunctionalTest
         $this->assertResponseCode();
         $this->assertEquals("/calendar/", $this->client->getRequest()->getRequestUri());
 
-        $this->em->refresh($this->calendar);
+        $calendar = $this->em->getRepository(Calendar::class)->find($calendar->id());
 
         /** @var Event $event */
         $event = $this->em->getRepository(Event::class)->findOneByTitle('Test weekly event for new calendar');
@@ -238,8 +243,6 @@ class DefaultControllerTest extends BaseFunctionalTest
         $this->client->submit($form);
         $this->assertResponseCode();
         $this->assertEquals("/calendar/", $this->client->getRequest()->getRequestUri());
-
-        $this->em->refresh($event);
 
         $event = $this->em->getRepository(Event::class)->findOneByTitle('some-single-test-event-changed');
 
@@ -290,8 +293,6 @@ class DefaultControllerTest extends BaseFunctionalTest
         $this->assertResponseCode();
         $this->assertEquals("/calendar/", $this->client->getRequest()->getRequestUri());
 
-        $this->em->refresh($event);
-
         $event = $this->em->getRepository(Event::class)->findOneByTitle('some-single-test-event-changed');
 
         $this->assertCount(1, $event->occurrences());
@@ -321,13 +322,20 @@ class DefaultControllerTest extends BaseFunctionalTest
             "update_event[title]" => "some-weekly-test-event-changed",
         ]);
 
-        $form["update_event[repetitionDays]"][0]->tick();
+        $form["update_event[repetitionDays]"][0]->tick(); // monday
         $form["update_event[repetitionDays]"][1]->untick();
-        $form["update_event[repetitionDays]"][2]->tick();
+        $form["update_event[repetitionDays]"][2]->tick(); // wednesday
         $form["update_event[repetitionDays]"][3]->untick();
-        $form["update_event[repetitionDays]"][4]->tick();
+        $form["update_event[repetitionDays]"][4]->tick(); // friday
         $form["update_event[repetitionDays]"][5]->untick();
         $form["update_event[repetitionDays]"][6]->untick();
+
+        // expected days:
+        //            2.9.2015  4.9.2015
+        //  7.9.2015  9.9.2015 11.9.2015
+        // 14.9.2015 16.9.2015 18.9.2015
+        // 21.9.2015 23.9.2015 25.9.2015
+        // 28.9.2015 30.9.2015
 
         $this->client->submit($form);
         $this->assertResponseCode();
@@ -338,17 +346,17 @@ class DefaultControllerTest extends BaseFunctionalTest
         $event = $this->em->getRepository(Event::class)->findOneById($event->id());
 
         $this->assertEquals('some-weekly-test-event-changed', $event->title());
-        $this->assertCount(14, $event->occurrences());
+        $this->assertCount(13, $event->occurrences());
 
-        $this->assertCount(1, $event->occurrences()->filter(function(Occurrence $occurrence){
-            return $occurrence->isDeleted();
-        }));
+//        $this->assertCount(1, $event->occurrences()->filter(function(Occurrence $occurrence){
+//            return $occurrence->isDeleted();
+//        }));
 
         $this->assertCount(13, $event->occurrences()->filter(function(Occurrence $occurrence){
             return !$occurrence->isDeleted();
         }));
 
-        $this->assertCount(1, $this->calendar->events());
+        $this->assertCount(1, $event->calendar()->events());
 
         $this->assertEquals(60, $event->duration()->minutes());
         $this->assertEquals('weekly', $event->type()->type());
@@ -356,40 +364,32 @@ class DefaultControllerTest extends BaseFunctionalTest
         $this->assertEquals("2015-09-01 16:00", $event->startDate()->format(self::FORMAT_DATETIME));
         $this->assertEquals("2015-09-30 17:00", $event->endDate()->format(self::FORMAT_DATETIME));
 
-        $this->assertEquals("2015-09-01 12:00", $event->occurrences()->get(0)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-01 13:30", $event->occurrences()->get(0)->endDate()->format(self::FORMAT_DATETIME));
-        $this->assertTrue($event->occurrences()->get(0)->isDeleted());
-
-        $this->assertEquals("2015-09-02 16:00", $event->occurrences()->get(1)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-02 17:00", $event->occurrences()->get(1)->endDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-04 16:00", $event->occurrences()->get(2)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-04 17:00", $event->occurrences()->get(2)->endDate()->format(self::FORMAT_DATETIME));
-
-        $this->assertEquals("2015-09-07 16:00", $event->occurrences()->get(3)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-07 17:00", $event->occurrences()->get(3)->endDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-09 16:00", $event->occurrences()->get(4)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-09 17:00", $event->occurrences()->get(4)->endDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-11 16:00", $event->occurrences()->get(5)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-11 17:00", $event->occurrences()->get(5)->endDate()->format(self::FORMAT_DATETIME));
-
-        $this->assertEquals("2015-09-14 16:00", $event->occurrences()->get(6)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-14 17:00", $event->occurrences()->get(6)->endDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-16 16:00", $event->occurrences()->get(7)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-16 17:00", $event->occurrences()->get(7)->endDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-18 16:00", $event->occurrences()->get(8)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-18 17:00", $event->occurrences()->get(8)->endDate()->format(self::FORMAT_DATETIME));
-
-        $this->assertEquals("2015-09-21 16:00", $event->occurrences()->get(9)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-21 17:00", $event->occurrences()->get(9)->endDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-23 16:00", $event->occurrences()->get(10)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-23 17:00", $event->occurrences()->get(10)->endDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-25 16:00", $event->occurrences()->get(11)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-25 17:00", $event->occurrences()->get(11)->endDate()->format(self::FORMAT_DATETIME));
-
-        $this->assertEquals("2015-09-28 16:00", $event->occurrences()->get(12)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-28 17:00", $event->occurrences()->get(12)->endDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-30 16:00", $event->occurrences()->get(13)->startDate()->format(self::FORMAT_DATETIME));
-        $this->assertEquals("2015-09-30 17:00", $event->occurrences()->get(13)->endDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-02 16:00", $event->occurrences()->get(0)->startDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-02 17:00", $event->occurrences()->get(0)->endDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-04 16:00", $event->occurrences()->get(1)->startDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-04 17:00", $event->occurrences()->get(1)->endDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-07 16:00", $event->occurrences()->get(2)->startDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-07 17:00", $event->occurrences()->get(2)->endDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-09 16:00", $event->occurrences()->get(3)->startDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-09 17:00", $event->occurrences()->get(3)->endDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-11 16:00", $event->occurrences()->get(4)->startDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-11 17:00", $event->occurrences()->get(4)->endDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-14 16:00", $event->occurrences()->get(5)->startDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-14 17:00", $event->occurrences()->get(5)->endDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-16 16:00", $event->occurrences()->get(6)->startDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-16 17:00", $event->occurrences()->get(6)->endDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-18 16:00", $event->occurrences()->get(7)->startDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-18 17:00", $event->occurrences()->get(7)->endDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-21 16:00", $event->occurrences()->get(8)->startDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-21 17:00", $event->occurrences()->get(8)->endDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-23 16:00", $event->occurrences()->get(9)->startDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-23 17:00", $event->occurrences()->get(9)->endDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-25 16:00", $event->occurrences()->get(10)->startDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-25 17:00", $event->occurrences()->get(10)->endDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-28 16:00", $event->occurrences()->get(11)->startDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-28 17:00", $event->occurrences()->get(11)->endDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-30 16:00", $event->occurrences()->get(12)->startDate()->format(self::FORMAT_DATETIME));
+        $this->assertEquals("2015-09-30 17:00", $event->occurrences()->get(12)->endDate()->format(self::FORMAT_DATETIME));
     }
 
     /**
--- prepare.sh
+++ prepare.sh
@@ -3,10 +3,11 @@
 php -r "readfile('https://getcomposer.org/installer');" | php
 php composer.phar install
 
-./console.sh assets:install Tests/app --symlink
-php ./Tests/app/console.php cache:clear --env=test
+php ./Tests/app/console.php assets:install Tests/app --symlink
+php ./Tests/app/console.php cache:clear --env=dev
 php ./Tests/app/console.php doctrine:schema:drop --force
 php ./Tests/app/console.php doctrine:schema:create
 php ./Tests/app/console.php doctrine:fixtures:load -n -vvv --fixtures=./Tests/DataFixtures/ORM
+php ./Tests/app/console.php server:start localhost:8080 --docroot=Tests/app/
 
-php -S localhost:8080 -t Tests/app/
+echo "Open in browser http://localhost:8080/web.php/calendar"
